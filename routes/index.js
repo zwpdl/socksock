0ba1433ab732149e21e90f0806f3212d31c05286
zwpdl <zwpdl@naver.com>
2016-07-28 01:37:19 +0900

    fatal: ambiguous argument 'HEAD': unknown revision or path not in the working tree. Use '--' to separate paths from revisions, like this: 'git <command> [<revision>...] -- [<file>...]'

---
+++ routes/index.js
@@ -0,0 +1,47 @@
+
+/*
+ * GET home page.
+ */
+
+var express = require('express');
+var app = express();
+
+
+exports.index = function(req, res){
+  res.render('index', { title: 'Express' });
+};
+
+
+
+app.post('/pushData', function(req, res){
+
+	var chunk = '';
+
+	//데이터를 가져옵니다.
+
+	req.on('data', function(data){
+
+	//데이터를 JSON으로 파싱합니다.
+
+	chunk = JSON.parse(data);
+
+	});
+
+	req.on('end',function(){
+
+	//파싱된 데이터를 확인합니다.
+
+	console.log("name : "+chunk.name + " , phone : "+chunk.phone);
+
+	});
+
+
+	// 아래의 OK라는 내용이 안드로이드의 ReadBuffer를 통해
+
+	// result String으로 바뀝니다.
+
+	res.write("OK");
+
+	res.end();
+
+	});
