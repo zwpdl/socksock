91e3093b6faa4f2ee82b0d9206ff8055f5cc740b
zwpdl <zwpdl@naver.com>
2016-08-12 09:36:11 +0900

    no message

--- app.js
+++ app.js
@@ -1,30 +1,12 @@
+var express = require('express');
 
-/**
- * Module dependencies.
- */
+var app = express.createServer(express.logger());
 
-var express = require('express')
-  , routes = require('./routes')
-  , user = require('./routes/user')
-  , http = require('http')
-  , path = require('path');
-
-var app = express();
-
-// all environments
-app.set('port', process.env.PORT || 3000);
-app.set('views', __dirname + 'views');
-app.set('view engine', 'jade');
-
-
-app.all('*',function(req, res){
-	res.render(
-	'index',
-	{msg: 'welcome to the party'}
-	);
-	
+app.get('/', function(request, response) {
+  response.send('Hello World!');
 });
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express.js server listening on port ' + app.get('port'));
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
 });
