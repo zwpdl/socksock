647fa1cc338a1a28c3b703eed8ca1901bd410d29
zwpdl <zwpdl@naver.com>
2016-08-12 09:13:04 +0900

    no message

--- app.js
+++ app.js
@@ -16,23 +16,21 @@ app.set('port', process.env.PORT || 3000);
 app.listen(process.env.PORT || 8080, function(){
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
-app.set('views', __dirname + '/views');
+app.set('views', __dirname + 'views');
 app.set('view engine', 'jade');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
 
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
+
+app.all('*',function(req, res){
+	res.render(
+	'index',
+	{msg: 'welcome to the party'}
+	);
+	
+});
 
 app.get('/', routes.index);
 app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+  console.log('Express.js server listening on port ' + app.get('port'));
 });
