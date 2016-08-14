053b7c1c5a67a66eda5b79a56e69033b37ed72e5
zwpdl <zwpdl@naver.com>
2016-08-14 19:51:07 +0900

    no message

--- app.js
+++ app.js
@@ -6,6 +6,43 @@ app.get('/', function(request, response) {
   response.send('Hello World!');
 });
 
+app.post('/pushData', function(req, res){
+
+	var chunk = '';
+
+	//데이터를 가져옵니다.
+
+	req.on('data', function(data){
+
+	//데이터를 JSON으로 파싱합니다.
+
+	chunk = JSON.parse(data);
+
+	});
+
+	req.on('end',function(){
+
+	//파싱된 데이터를 확인합니다.
+
+	//console.log("name : "+chunk.name + " , phone : "+chunk.phone);
+
+		response.send("name : "+chunk.name + " , phone : "+chunk.phone);
+		
+		
+	});
+
+
+	// 아래의 OK라는 내용이 안드로이드의 ReadBuffer를 통해
+
+	// result String으로 바뀝니다.
+
+	res.write("OK");
+
+	res.end();
+
+	});
+
+
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
